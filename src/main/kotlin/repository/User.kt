f134877415d4482156f8f93f3c5c4901cfef61e4
Piotr Wolak <contact@codersee.com>
2025-03-25 20:10:52 +0100

    Init project

---
+++ src/main/kotlin/repository/User.kt
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime
+import java.util.*
+
+data class User(
+    val id: String = UUID.randomUUID().toString(),
+    val firstName: String,
+    val lastName: String,
+    val enabled: Boolean,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+)
